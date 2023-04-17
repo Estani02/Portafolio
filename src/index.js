83d64bf7c054311778c5ae2bedef3713084bde45
Estani02 <estaniolmedo@gmail.com>
2023-04-16 21:54:59 -0300

    translation implementation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import en_global from './translation/en/global.json';
+import es_global from './translation/es/global.json';
 
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'es',
+  resources: {
+    es: {
+      global: es_global
+    },
+    en: {
+      global: en_global
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
