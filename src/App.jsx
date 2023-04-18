b8ef27540ec6933ea402d152a3c4d3a3bd33205c
Estani02 <estaniolmedo@gmail.com>
2023-04-18 18:42:16 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
-import NavBar from './components/NavBar/NavBar';
 import { Pdf } from './components/Pdf/index.jsx';
 
 function App() {
 
-  const location = useLocation();
 
   return (
     <div className="App">
-      {/* {location.pathname === '/' ? undefined : <NavBar />} */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cv-estanislao-olmedo' element={<Pdf />} />
