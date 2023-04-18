619c84958daf483dbb7de0230c0fa609f7731e08
Estani02 <estaniolmedo@gmail.com>
2023-04-18 16:44:57 -0300

    feat: routes aggregate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/home/Home';
 import NavBar from './components/NavBar/NavBar';
+import { Pdf } from './components/Pdf/index.jsx';
 
 function App() {
+
+  const location = useLocation();
+
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+      {/* {location.pathname === '/' ? undefined : <NavBar />} */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cv-estanislao-olmedo' element={<Pdf />} />
+      </Routes>
+      
     </div>
   );
 }
